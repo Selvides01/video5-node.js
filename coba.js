8d066476625aca6eea87c58c81c7327c62acec99
Selvides01 <selvides16266@gmail.com>
2024-04-04 15:15:49 +0700

    initial commit

---
+++ coba.js
@@ -0,0 +1,5 @@
+function cetakNama(nama) {
+    return `Hallo, nama saya ${nama}`;
+}
+
+module.exports = cetakNama;
---
+++ index.js
@@ -0,0 +1,9 @@
+// const nama = 'Selvi Desti Riyani';
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+const cetaknama = require('./coba');
+
+//console.log('Hello WPU');
+console.log(cetakNama('Selvi Desti Riyani')) ;
+
