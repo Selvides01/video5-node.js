ea58ffced6f300c66df61bf96c3462f7a401b043
Selvides01 <selvides16266@gmail.com>
2024-04-17 10:55:20 +0700

    initial commit

--- coba.js
+++ coba.js
@@ -2,4 +2,20 @@ function cetakNama(nama) {
     return `Hallo, nama saya ${nama}`;
 }
 
-module.exports = cetakNama;
+const PI = 3.14;
+
+const mahasiswa = {
+    nama: 'Doddy Ferdiansyah',
+    umur: 20, // tambahkan koma di sini
+    cetakMhs() {
+        return `Halo, nama saya ${this.nama} dan saya ${this.umur} tahun`;
+    }
+};
+
+class Orang {
+    constructor() { // tambahkan tanda kurung buka dan tutup di sini
+       console.log('Objek orang telah dibuat!!');
+    }
+}
+
+module.exports = { cetakNama, PI, mahasiswa, Orang };
--- index.js
+++ index.js
@@ -2,8 +2,18 @@
 // const cetakNama = (nama) => `Hi, nama saya ${nama}`;
 // console.log(cetakNama(nama));
 
-const cetaknama = require('./coba');
+// const fs = require('fs'); // core module
+// const cetakNama = require('./coba'); //local module 
+// const moment = require('moment'); // third party module / npm module / node_modules
 
-//console.log('Hello WPU');
-console.log(cetakNama('Selvi Desti Riyani')) ;
+// console.log('Hello WPU');
 
+// const cetakNama = require('./coba');
+// const PI = require('./coba');
+const coba = require('./coba');
+
+console.log(
+coba.cetakNama('Selvi Desti Riyani'), 
+clearcoba.PI,
+coba.mahasiswa.cetakMhs(), 
+new coba.Orang());
